e6c2df185a7cd597a7e8ce611873a3966fd77890
mikailtal <jeevanpunalur@gmail.com>
2023-01-13 15:55:31 +0530

    second

---
+++ script.js
@@ -0,0 +1,75 @@
+// var slideIndex = 1;
+//
+//
+// showDivs(slideIndex);
+//
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+//
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1)
+//   {slideIndex = x.length;
+//
+//   }
+//
+//
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//
+//   x[slideIndex-1].style.display = "block";
+// console.log(slideIndex-1);
+// }
+
+//
+$(document).ready(function(){
+$(".b").click(function(){
+    $(this).toggleClass("b");
+    $(this).toggleClass("b-selected");
+ });
+});
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellalign: 'right',
+  pageDots: false,
+  groupCells: '20%',
+  selectedAttraction: 0.03,
+  friction: 0.15
+});
+
+var flkty = new Flickity( '.carousel', {
+  // options
+});
+
+
+
+
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
